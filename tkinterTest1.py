5603c0fd66cae3b17d6f1a2cc1236719b546f443
Alessandro <33810424+ACINIELLO@users.noreply.github.com>
2020-08-15 13:30:37 -0400

    Delete tkinterTest1.py

--- tkinterTest1.py
+++
@@ -1,6 +0,0 @@
-from tkinter import *
-
-root = Tk()
-l = Label(root,text="yeeet")
-l.pack() # pack just packs the label into the window (wherever theres room)
-root.mainloop() # makes sure thing displays for a goo amount of time as computer runs files quick
